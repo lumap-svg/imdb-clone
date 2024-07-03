fff5132e8cf275b39bed0d6ef17a864c29146bce
lumap-svg <lutanywapeter@gmail.com>
2024-07-03 06:31:19 -0700

    Added the about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl p-3 mx-auto space-y-4">
+      <h1 className="text-amber-500 text-xl font-medium">About</h1>
+      <p>
+        ChatGPT IMDb, or the Internet Movie Database, is a comprehensive online
+        repository of information related to films, television series, actors,
+        directors, and other industry professionals. Founded in 1990, IMDb has
+        grown to become one of the most trusted sources for entertainment
+        content worldwide. Users can access a vast database containing details
+        on movies from various genres, release dates, plot summaries, and
+        trivia. Each film or show entry typically includes user-generated
+        ratings and reviews, helping viewers make informed decisions about what
+        to watch next
+      </p>
+      <p>
+        One of IMDb's standout features is its user-generated content, allowing
+        registered members to contribute to the site's database. Users can rate
+        movies, write reviews, compile personal watchlists, and participate in
+        discussion forums. This collaborative approach has made IMDb not just a
+        resource for information but also a community where film enthusiasts can
+        engage with each other and share their opinions and knowledge about
+        movies and TV shows.
+      </p>
+      <p>
+        In addition to its website, IMDb has expanded its presence to include
+        mobile apps and streaming services, making it easier for users to access
+        information on the go. Over the years, IMDb has become a go-to platform
+        for anyone looking to explore the world of cinema, whether they're
+        casual viewers, industry professionals, or hardcore film buffs seeking
+        detailed information about their favorite movies and actors.
+      </p>
+    </div>
+  );
+};
+
+export default About;
