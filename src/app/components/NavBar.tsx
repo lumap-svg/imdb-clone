107fdaef6416ea5a2425f7aa1538552526ad49b8
lumap-svg <lutanywapeter@gmail.com>
2024-07-04 02:16:29 -0700

    created NavBar and NavBarItem Components

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import NavbarItem from "./NavbarItem";
 
 const NavBar = () => {
-  return <div>Navigation tab</div>;
+  return (
+    <div className="flex gap-6 items-center justify-center max-w-6xl mx-auto p-4 lg:text-lg bg-amber-200 dark:bg-gray-600">
+      <NavbarItem title="Trending" param="fetchTrending" />
+      <NavbarItem title="Top Rated" param="fetchTopRated" />
+    </div>
+  );
 };
 
 export default NavBar;
